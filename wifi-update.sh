cc0416f69a2d9aa03df9264edd055f09af80a139
Grayson Earle <graysonearle@gmail.com>
2026-07-06 11:03:20 +0200

    fix: ignore SSL verification in wifi-update.sh for offline clock drift

--- wifi-update.sh
+++ wifi-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Temporarily disable eth0 so git pull uses WiFi (for Pis with Ethernet-only switch).
 sudo ip link set eth0 down
-git pull
+git -c http.sslVerify=false pull
 sudo ip link set eth0 up
+
